e0ee42e45d517aefcab690b3238af0c32cc86b35
michalPrzyl <michal.przyl@gmail.com>
2024-01-12 00:43:13 +0100

    oop

--- migrator.py
+++ migrator.py
@@ -3,18 +3,19 @@ import os
 
 class Migrator:
     already_applied = ['0003_upie']
+    migration_files = []
+    repetitions = []
 
-    
     def __init__(self,
                  directory: str) -> None:
         self.directory = directory
 
     def inspect_directory(self):
-        files: list[str] = os.listdir(self.directory)
-        reps = self.__get_repetitions(files)
+        self.migration_files: list[str] = os.listdir(self.directory)
+        self.repetitions: list[str] = self.__get_repetitions(self.files)
 
-    def __get_repetitions(self, migration_files: list):
-        prefixes = [file[:4] for file in migration_files]
+    def __get_repetitions(self):
+        prefixes = [file[:4] for file in self.migration_files]
         repetitions = set()
 
         if len(prefixes) != len(set(prefixes)):
@@ -22,9 +23,7 @@ class Migrator:
                 if prefixes.count(prefix) > 1:
                     repetitions.add(prefix)
 
-        # print(prefixes)
-        # print(repetitions)
-
-        for repetition in repetitions:
-            same_pref_reps = [file for file in migration_files if file.startswith(repetition)]
+    def foo(self):
+        for repetition in self.repetitions:
+            same_pref_reps = [file for file in self.migration_files if file.startswith(repetition)]
             print(same_pref_reps)
