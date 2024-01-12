da81758de1b9969bede01f8cf879ddccd7df7130
michalPrzyl <michal.przyl@gmail.com>
2024-01-12 02:02:43 +0100

    typo

--- migrator.py
+++ migrator.py
@@ -50,7 +50,7 @@ class Migrator:
     
     def fix_repetitions(self):
         max_applied_prefix = max(self.get_applied_prefixes())
-        file_to_fix = [unapplied_migration for unapplied_migration in self.unapplied if unapplied_migration[:4] == max_applied_prefix][0]
+        file_to_fix = [unapplied_migration for unapplied_migration in self.unapplied if int(unapplied_migration[:4]) == max_applied_prefix][0]
         new_name = 'abc'
         self.rename_file(file_to_fix, new_name)
         
